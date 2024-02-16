7c8625ea114e18875dc7d395f01b756ad2749ea8
JeremiahYoder <Jeremiah.yoder@hotmail.com>
2024-02-16 12:15:34 -0700

    added useAxios hook

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAxios = (url: string) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        let isMounted = true;
+
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(url);
+                if (isMounted) {
+                    setData(response.data);
+                }
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            isMounted = false;
+        };
+    }, [url]);
+
+    return { data, loading, error };
+};
+
+export default useAxios;
