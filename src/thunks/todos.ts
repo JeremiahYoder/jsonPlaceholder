4d3f8827283e0b13dd69804fbc2882f589f97804
JeremiahYoder <Jeremiah.yoder@hotmail.com>
2024-02-20 10:52:33 -0700

    complete todos

--- src/thunks/todos.ts
+++ src/thunks/todos.ts
@@ -1,6 +1,6 @@
-import { getTodos } from "../api/todos";
+import { getTodoById, getTodos } from "../api/todos";
 import { todos } from "../selectors/todos";
-import { loadTodos } from "../slices/todosSlice";
+import { clearTodos, loadTodo, loadTodos, resetTodos } from "../slices/todosSlice";
 import { AppDispatch, RootState } from "../store";
 
 export function loadTodosData() {
@@ -13,3 +13,18 @@ export function loadTodosData() {
         })
     }
 }
+
+export function loadTodoDataById(id: number) {
+    return (dispatch: AppDispatch, getState: () => RootState) => {
+        const currTodos = todos(getState())
+        if (currTodos.findIndex(todo => todo.id === id) !== -1) return
+
+        getTodoById(id.toString()).then(data => {
+            if (data) dispatch(loadTodo(data))
+        })
+    }
+}
+
+export const resetTodoData = () => (dispatch: AppDispatch) => dispatch(resetTodos())
+
+export const clearTodoData = () => (dispatch: AppDispatch) => dispatch(clearTodos())
