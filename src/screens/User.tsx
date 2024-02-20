d5d230d720986a82b6a9cfd771ca3f899c5f0adc
JeremiahYoder <Jeremiah.yoder@hotmail.com>
2024-02-20 13:22:45 -0700

    cleanup

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import SafeAreaView from '../components/SafeAreaView'
 import useAppDispatch from '../hooks/useAppDispatch'
-import { unloadCurrentUser } from '../thunks/users'
 import useAppSelector from '../hooks/useAppSelector'
 import { currentUser } from '../selectors/users'
 import { currentUsersPosts } from '../selectors/posts'
 import { currentUsersAlbums } from '../selectors/albums'
 import { currentUsersTodos } from '../selectors/todos'
+import { unloadCurrentUser } from '../thunks/users'
 
 const User = () => {
     const dispatch = useAppDispatch()
@@ -16,19 +16,37 @@ const User = () => {
     const Albums = useAppSelector(currentUsersAlbums)
     const Todos = useAppSelector(currentUsersTodos)
 
+    useEffect(() => {
+        () => {
+            dispatch(unloadCurrentUser())
+        }
+    }, [])
+
+    if (!User) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View>
+                    <Text style={{ fontWeight: 'bold' }}>
+                        No User Loaded
+                    </Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text style={{ fontWeight: 'bold' }}>User Details</Text>
+                <Text style={styles.title}>User Details</Text>
                 <Text>Name: {User.name}</Text>
                 <Text>Username: {User.username}</Text>
                 <Text>Email: {User.email}</Text>
             </View>
-            <View style={{ height: 20 }} />
-            <View style={{ width: '100%', height: 100, alignItems: 'center' }}>
-                <Text style={{ fontWeight: 'bold' }}>User Posts: {Posts.length}</Text>
-                <Text style={{ fontWeight: 'bold' }}>User Albums: {Albums.length}</Text>
-                <Text style={{ fontWeight: 'bold' }}>User Todos: {Todos.length}</Text>
+            <View style={styles.spacer} />
+            <View style={styles.relationsContainer}>
+                <Text style={styles.title}>User Posts: {Posts?.length}</Text>
+                <Text style={styles.title}>User Albums: {Albums?.length}</Text>
+                <Text style={styles.title}>User Todos: {Todos?.length}</Text>
             </View>
         </SafeAreaView>
     )
@@ -39,6 +57,17 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    spacer: {
+        height: 20
+    },
+    title: {
+        fontWeight: 'bold'
+    },
+    relationsContainer: {
+        width: '100%',
+        height: 100,
+        alignItems: 'center'
     }
 })
 
