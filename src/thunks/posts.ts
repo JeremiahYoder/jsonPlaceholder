0440d34a7af0619371c882d515e265ce40b128d8
JeremiahYoder <Jeremiah.yoder@hotmail.com>
2024-02-20 09:49:54 -0700

    post thunks

--- src/thunks/posts.ts
+++ src/thunks/posts.ts
@@ -1,6 +1,6 @@
-import { getPosts } from "../api/posts";
+import { getPostById, getPosts } from "../api/posts";
 import { posts } from "../selectors/posts";
-import { loadPosts } from "../slices/postsSlice";
+import { loadPosts, loadPost, resetPosts, clearPosts } from "../slices/postsSlice";
 import { AppDispatch, RootState } from "../store";
 
 export function loadPostsData() {
@@ -17,3 +17,18 @@ export function loadPostsData() {
         })
     }
 }
+
+export function loadPostDataById(id: number = 1) {
+    return (dispatch: AppDispatch, getState: () => RootState) => {
+        const currPosts = posts(getState())
+        if (currPosts.findIndex(user => user.id === id) !== -1) return
+
+        getPostById(id?.toString()).then(data => {
+            if (data) dispatch(loadPost(data))
+        })
+    }
+}
+
+export const resetPostData = () => (dispatch: AppDispatch) => dispatch(resetPosts())
+
+export const clearPostData = () => (dispatch: AppDispatch) => dispatch(clearPosts())
