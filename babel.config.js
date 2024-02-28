6ea2d072ed565de051ab1098069e719a8762ba98
JeremiahYoder <Jeremiah.yoder@hotmail.com>
2024-02-28 12:11:02 -0700

    expo, metro, babel config enhancements

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
+const MODULE_RESOLVER = [
+  'module-resolver',
+  {
+    extension: ['.js', '.ts', '.ios.js', '.ios.ts', '.android.js', '.android.ts', '.json'],
+    alias: {
+      '@Components': './src/components',
+      '@Navigation': './src/navigation'
+    }
+  }
+]
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-  ]
+  plugins: ['react-native-reanimated/plugin']
 };
